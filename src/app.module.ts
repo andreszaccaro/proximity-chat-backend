72887fdb6fdbfcaf565643ba84b400560447c177
andreszaccaro <andreszaccaro@gmail.com>
2021-01-08 18:28:34 -0500

    Configure Config, Graphql and PubSub modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { PubSubModule } from './resolvers/pubSub/pubSub.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot({
+      typePaths: ['src/**/*.graphql'],
+      installSubscriptionHandlers: true,
+      context: ({ req }) => ({ req }),
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [PubSubModule],
 })
 export class AppModule {}
---
+++ src/graphql.typings.ts
@@ -0,0 +1,15 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export abstract class IQuery {
+    abstract getMessage(): Response | Promise<Response>;
+}
+
+export class Response {
+    message?: string;
+}
